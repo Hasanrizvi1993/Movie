c0fd8a8d1c6003fcc280351f1ad9d314e8eab7e1
hasanrizvi1993 <hasanrizvi1993@gmail.com>
2022-01-29 11:22:42 -0800

    added pictures

--- data/movies.js
+++ data/movies.js
@@ -1,12 +1,16 @@
 const movies = [
     {
         name: 'Terminator',
-        releaseDate: '01/01/91',
+        releaseDate: '10/26/1984',
+        description: 'Disguised as a human, a cyborg assassin known as a Terminator (Arnold Schwarzenegger) travels from 2029 to 1984 to kill Sarah Connor (Linda Hamilton). Sent to protect Sarah is Kyle Reese (Michael Biehn), who divulges the coming of Skynet, an artificial intelligence system that will spark a nuclear holocaust. Sarah is targeted because Skynet knows that her unborn son will lead the fight against them. With the virtually unstoppable Terminator in hot pursuit, she and Kyle attempt to escape.(Source: rottentomatoes.com)',
+        imageURL: 'https://en.wikipedia.org/wiki/The_Terminator#/media/File:Terminator1984movieposter.jpg'
     },
     {
         name: 'Titanic',
-        releaseDate: '01/02/97',
+        releaseDate: '12/19/1997',
+        description: 'The movie tells the dramatic story of the Titanic with Leonardo Di Caprio and Kate Winslet as the main actors.In 1912 a young poor guy, Leonardo, travels by the gorgeous ship Titanic from London to New York with a big dream. On board he meets a girl. He fells in love with her when he sees this gorgeous woman. However after a few days the ship hits an enormous iceberg and the tragedy begins to unfold. The Titanic starts sinking and the movie is very close to the real tragedy. It shows a beautiful love story with an extremely sad ending. The acting is first-rate. Leonardo Di Caprios and Kate Winslets performance is so brilliant that I cried during the whole movie.(Source: languages.ac.nz)',
     },
+    
 ]
 
 
