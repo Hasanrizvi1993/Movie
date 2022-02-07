e7d68b2fc544f5c195fe07839758069b9ca6168b
Moazam Hussain <moazam694@gmail.com>
2022-02-06 16:04:23 -0800

    added more actors and added movie

--- data/actors.js
+++ data/actors.js
@@ -5,6 +5,27 @@ const actors = [
         age: '74',
         image: 'public/440px-Arnold_Schwarzenegger_by_Gage_Skidmore_4.jpeg',
     },
+
+    {
+        name: 'Leonardo Dicaprio',
+        birthDay: '11/11/1974',
+        age: '47',
+        image: 'http://d17zbv0kd7tyek.cloudfront.net/wp-content/uploads/2015/06/leonardo-dicaprio-fb.jpg'
+    },
+
+    {
+        name: 'Tom Holland',
+        birthDay: '06/01/1996',
+        age: '25',
+        image: 'https://i.insider.com/6200008afa4f1f001826349c?width=700'
+    },
+
+    {
+        name: 'Dwayne Johnson',
+        birthDay: '05/02/1972',
+        age: '49',
+        image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dwayne-johnson-1619161347.jpg?crop=1.00xw:0.667xh;0,0.0194xh&resize=480:*'
+    }
     
 ]
 
